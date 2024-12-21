36a350273e1f3cb1062a2e69f21caa35747cf2b9
juan pablo martinez figueroa <jpmf818@gmail.com>
2024-12-21 15:20:08 -0500

    laboratorio 8 completado

--- pokedex/admin.py
+++ pokedex/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Pokemon
+from .models import Pokemon, Trainer
+
 # Register your models here.
 @admin.register(Pokemon)
-class Pokemonadmin(admin.Modeladmin):
+class Pokemonadmin(admin.ModelAdmin):
+    pass
+# Register your models here.
+@admin.register(Trainer)
+class TrainerAdmin(admin.ModelAdmin):
     pass
